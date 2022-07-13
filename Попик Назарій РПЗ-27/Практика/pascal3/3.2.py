22eb6dc00d6d888c0be336a812b9b97b95ba8f72
Windows-1 <51761266+Windows-1@users.noreply.github.com>
2022-07-13 21:34:38 +0300

    Update 3.2.py

--- Попик Назарій РПЗ-27/Практика/pascal3/3.2.py
+++ Попик Назарій РПЗ-27/Практика/pascal3/3.2.py
@@ -1,16 +1,32 @@
 # 3.2 варіант 14. Відомо, що з чотирьох чисел a1, a2, a3, a4 одне відрізняється від
 # інших трьома рівними між собою, присвоїти номер цього числа змінній n.
+import sys
 
-numbers = [7, 7, 7, 3]
+numbers = []
 n = 0
 
+for i in range(4):
+    numbers.append(input('Введань число:'))
 
-if numbers[1]==numbers[2] and numbers[2]==numbers[3]:
+if numbers[1]==numbers[2]==numbers[3]:
     n = 1
-elif numbers[0]==numbers[2] and numbers[2]==numbers[3]:
+elif numbers[0]==numbers[2]==numbers[3]:
     n = 2
-elif numbers[0]==numbers[1] and numbers[1]==numbers[3]:
+elif numbers[0]==numbers[1]==numbers[3]:
     n = 3
 else: 
     n = 4
+
+count = 0
+if n!=1:
+    a = numbers[0]
+else:
+    a = numbers[1]
+for i in numbers:
+    if i == a:
+        count+=1
+if count!=3:
+    print('Два чи більше чисел різні або всі числа однакові')
+    sys.exit(0)
+
 print('Номер числа:', n)
