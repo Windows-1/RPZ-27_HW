1f95e6af9098b8e81fd63db31aa240e8bb44ce85
Windows-1 <51761266+Windows-1@users.noreply.github.com>
2022-07-14 20:59:17 +0300

    Update 5.1.py

--- Попик Назарій РПЗ-27/Практика/pascal5/5.1.py
+++ Попик Назарій РПЗ-27/Практика/pascal5/5.1.py
@@ -1,14 +1,25 @@
 # 5.1 варіант 13. Дана степінь натурального числа A. Знайти її показник.
+import sys
+
 try:
-    a = int(input('Введіть число:'))
-    n = int(input('Введіть степінь:'))
-    b = a
-    i = 2
-
-    while i <= n:
-        b = b*a
-        i = i+1
-
-    print('Показник', a, 'в степені', n, '=', b)
+    a = int(input('Введіть початкове число:'))
+    b = int(input('Введіть результат:'))
+    n = a
+    count = 0
+    
+    if b==1:
+        print()
+    elif b<a:
+        sys.exit(0)
+    elif a>=2 and b>=2:
+        count+=1
+        while n!=b:
+            n = n * a
+            if n>b:
+                sys.exit(0)
+            count+=1
+    else:
+        sys.exit(0)
+    print('\n''Показник степіня', count)
 except:
-    print('Перевірте правельність написання числа та степеню')
+    print('\n''Перевірте правельність написання числа та результату')
