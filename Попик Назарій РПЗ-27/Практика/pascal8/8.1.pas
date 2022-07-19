c54f2117cc50068a4c258d7850eb9068446dea3f
Windows-1 <51761266+Windows-1@users.noreply.github.com>
2022-07-19 18:20:36 +0300

    Update 8.1.pas

--- Попик Назарій РПЗ-27/Практика/pascal8/8.1.pas
+++ Попик Назарій РПЗ-27/Практика/pascal8/8.1.pas
@@ -2,10 +2,10 @@
 var a,b:string;
     i:byte;
 begin
-    write('строка A = ');readln(a);
-    write('строка B = ');readln(b);
+    write('рядок A = ');readln(a);
+    write('рядок B = ');readln(b);
     for i:=Length(a) downto 1 do
       while Pos(a[i],b)>0 do
         Delete(a,i,1);
-    writeln('Полученная строка = ',a);
+    writeln('Результат = ',a);
 end.
